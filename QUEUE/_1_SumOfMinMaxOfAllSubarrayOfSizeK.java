85fdd47f64d3b082969603ec8f8ec6d5a1f6cb57
BhanushreeNN <bhanushree1023@gmail.com>
2024-11-04 12:47:03 +0530

    add queue file 1

---
+++ QUEUE/_1_SumOfMinMaxOfAllSubarrayOfSizeK.java
@@ -0,0 +1,9 @@
+package QUEUE;
+
+import java.util.LinkedList;
+
+public class _1_SumOfMinMaxOfAllSubarrayOfSizeK {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+    }
+}
